f24a5d32e14b0277024670161c73699444834eb2
Vanya111222 <vanyagonchar505@gmail.com>
2024-11-11 14:06:23 +0200

    Firs commit

---
+++ zamina.py
@@ -0,0 +1,18 @@
+'''nub = (413)
+nub_two = (314)
+text = ("bebro")
+
+print(nub + nub_two)
+print(str(nub) + text)
+print("enter your password")
+password= input()
+print("your password!")
+print(password)'''
+
+num_1 = int(input("введіть число:"))
+num_2 = int(input("введіть число:"))
+num_3 = int(input("введіть число:"))
+
+sum =  num_1 * num_2 * num_3
+
+print ("sum numbers:", sum )
